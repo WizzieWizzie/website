e5e5b6f0746db7806b5fbb22c77668117248161a
flunder <larsf2005@gmail.com>
2015-07-16 12:45:03 +0100

    map address slider beuatification and testing / mobile nav fix

--- js/wizzie.js
+++ js/wizzie.js
@@ -391,13 +391,14 @@ WIZZ.owlSlider = function(){
     */
 
     $("section.map ul").owlCarousel({
-        items: 2,
+        items: 4,
         navigation: true,
         pagination: false,
-        itemsDesktop : [1000,2], //5 items between 1000px and 901px
+
         itemsDesktopSmall : [900,2], // betweem 900px and 601px
-        itemsTablet: [600,1], //2 items between 600 and 0
-        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+        itemsTablet: [600,1],        // 2 items between 600 and 0
+        itemsMobile : false,         // itemsMobile disabled - inherit from itemsTablet option
+        rewindNav:false
     });
 
 }
