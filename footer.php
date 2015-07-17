f19b81551db9ae0406a766bd5848ec3a3ed4a071
madebydavid <sutherland.dave@gmail.com>
2015-07-17 19:37:21 +0100

    child protection policy

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
                 <p>is a project of <a href="http://www.diversedigital.org/" target="_blank">Diverse Digital</a>, an educational<br/> technology charity</p>
 
                 <h4>Child Protection Policy</h4>
-                <p>View our <a href="/child-protection-policy">child protection policy</a>, including a<br/> Child Protection Incident Report Form</p>
+                <p>View our <a href="http://www.wizziewizzie.org/content/uploads/2015/07/DiverseDigitial-ChildProtectionPolicy-2013-10.pdf">child protection policy</a>, including a<br/> Child Protection Incident Report Form</p>
             </div>
 
             <p class="copyright">&copy;Reg. in England and Wales. Charity No. 1154372</p>
