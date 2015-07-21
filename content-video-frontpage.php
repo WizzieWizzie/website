69bb79c56cb4ef44c286fa139f0b02cfcc6c7a1b
flunder <larsf2005@gmail.com>
2015-07-21 17:31:42 +0100

    frontpage update

---
+++ content-video-frontpage.php
@@ -0,0 +1,21 @@
+<section class="two-col-feature gradient padding-alt-box font-smoothe" data-pattern="slash">
+
+    <div class="g-row">
+
+        <div class="module-quote-wrap cf v-center bg-white">
+
+            <div class="two-col-feature-col1 g-col-half bg-white v-centre-inner">
+                <h3>Come and a have a look</h3>
+                <p>Sed molestie augue sit amet leo consequat posuere. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
+</p>
+            </div>
+
+            <div class="two-col-feature-col2 g-col-half v-centre-inner">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/OjJ4JoIOUhg" frameborder="0" allowfullscreen></iframe>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
