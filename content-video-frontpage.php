aa0babe3855785fb3dbb14f7310c60a64ff7b08a
flunder <larsf2005@gmail.com>
2015-07-22 17:35:14 +0100

    editable video feature

--- content-video-frontpage.php
+++ content-video-frontpage.php
@@ -2,16 +2,17 @@
 
     <div class="g-row">
 
-        <div class="module-quote-wrap cf v-center bg-white">
+        <div class="two-col-feature-wrap cf v-center bg-white">
 
-            <div class="two-col-feature-col1 g-col-half bg-white v-centre-inner">
-                <h3>Come and a have a look</h3>
-                <p>Sed molestie augue sit amet leo consequat posuere. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-</p>
+            <div class="two-col-feature-col1 g-col-half bg-white v-centre-inner two-col-feature-arrow">
+                <h3><?php the_field('headline'); ?></h3>
+                <p class="marginless">
+                    <?php the_field('copy'); ?>
+                </p>
             </div>
 
             <div class="two-col-feature-col2 g-col-half v-centre-inner">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/OjJ4JoIOUhg" frameborder="0" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="<?php echo get_field('video_url'); ?>?showinfo=0" frameborder="0" allowfullscreen ></iframe>
             </div>
 
         </div>
