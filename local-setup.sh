643363097399cdfb4c7a884629eebdbb5f0f027b
madebydavid <sutherland.dave@gmail.com>
2015-07-22 19:13:55 +0100

    local setup just makes config - db and content will be synced - set vagrant project

--- local-setup.sh
+++ local-setup.sh
@@ -4,55 +4,23 @@
 
 domain="local.wizziewizzie.org"
 
-adminUser="root"
-adminPass="password"
-adminEmail="sutherland.dave@gmail.com"
-
-siteTitle="Wizzie Wizzie"
+dbName="Wizzie"
+dbUser="root"
+dbPass=""
+dbHost="localhost"
 
 ########################################
 
+# create the local config file
+cat << EOF > local-config.php
+<?php
 
-# get the list of plugins
-plugins=`ls -d content/plugins/*/ | sed "s/content\/plugins\/\([^\/]*\)\/.*/\1/g"`
-# get the theme
-theme=`ls -d content/themes/*/ | sed "s/content\/themes\/\([^\/]*\)\/.*/\1/g"`
+define('DB_NAME', '$dbName');
+define('DB_USER', '$dbUser');
+define('DB_PASSWORD', '$dbPass');
+define('DB_HOST', '$dbHost');
+define('WP_HOME', 'http://$domain/');
+define('WP_SITEURL', 'http://$domain/wordpress');
 
-cd vendor/wp-cli/wp-cli/bin
+EOF
 
-wp="./wp --path=../../../../wordpress --url=$domain"
-
-# reset and initialize wordpress
-$wp db reset
-$wp core install \
-        --admin_user="$adminUser" --admin_password="$adminPass" --admin_email="$adminEmail" \
-        --title="$siteTitle"
-
-# activate the plugins
-for plugin in $plugins
-do
-    $wp plugin activate $plugin
-done
-
-# activate the theme
-$wp theme activate $theme
-
-# advanced custom fields
-$wp acf import all
-
-# permalink structure - outputs an error which can be ignored
-$wp rewrite structure "/%postname%/" > /dev/null
-
-# delete hello world and sample page
-$wp post delete 1
-$wp post delete 2
-
-
-# import data
-$wp import ../../../../content/data/wizziewizzie.wordpress.2015-06-25.xml --authors=create
-
-# Add default Home and News page
-# Relies on the import data ids to be correct
-$wp option set show_on_front page
-$wp option set page_on_front 10
-$wp option set page_for_posts 12
