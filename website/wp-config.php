e6766e7b63d2ba8f7f2a994de228c5015e93b12e
David Sutherland <sutherland.dave@gmail.com>
2017-02-19 17:17:48 +0000

    salts only specified here for dev

--- website/wp-config.php
+++ website/wp-config.php
@@ -11,6 +11,20 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 	define( 'DB_HOST', 'localhost' );
 	define( 'WP_HOME', 'http://localhost:8080');
 	define( 'WP_SITEURL', 'http://localhost:8080/wordpress');
+
+	// ==============================================================
+	// Salts, for security
+	// Grab these from: https://api.wordpress.org/secret-key/1.1/salt
+	// ==============================================================
+	define('AUTH_KEY',         'HzHHZ}Wnm)KaMl]Yy8e:0/_sSwx-*K?=@#;<M8nejf,~k+5UEjZtax~vE;nuFO_o');
+	define('SECURE_AUTH_KEY',  '+]<WhWgEP5|W-@>?y+K6oP!e*A8{wc<+;{B0ePePwKR+.;*yoN5cuY@-+B3`7Oov');
+	define('LOGGED_IN_KEY',    '_lkwmW&j##tT.+[S]6{Ea$*}+hGQ`HRZjRJZU[S?Ks*z8ij K>T@A17#hz+mNZ/(');
+	define('NONCE_KEY',        'au$+5R+<tbhs[s,iBGfFR)cf-c^}Y9L{L>]vR-?UgL|i#7!IQVd=IR7e!?Z=SY[g');
+	define('AUTH_SALT',        'VO!E+z.l$h8z7+i-+bkhKv^:^[}-zGhAic]}8h{g6$Cg>&>_luqz+1#d[I0YN.XL');
+	define('SECURE_AUTH_SALT', '|vsDlvhxmFnB8~_@ro|_ t_hU^b e(pn&4~~:g`T|pt}kc]MTEJV-a5WP,.fM<,5');
+	define('LOGGED_IN_SALT',   'p^:`]8rz&rE-7]L({.|d[Yt=3lQnq<SmMWM Jz#nW-r`jZW4q:U31mdl3q[|HM`{');
+	define('NONCE_SALT',       '=3v/c^LrPGd{s|,B[QR%IZ/a22dR]s_Wb393bciKEa<Qdi#@l>OfX-a(A&sDz*1s');
+
 }
 
 // ========================
@@ -24,19 +38,6 @@ define( 'WP_CONTENT_URL', WP_HOME . '/content' );
 // ================================================
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
-
-// ==============================================================
-// Salts, for security
-// Grab these from: https://api.wordpress.org/secret-key/1.1/salt
-// ==============================================================
-define('AUTH_KEY',         '***REMOVED***');
-define('SECURE_AUTH_KEY',  '***REMOVED***');
-define('LOGGED_IN_KEY',    '***REMOVED***');
-define('NONCE_KEY',        '***REMOVED***');
-define('AUTH_SALT',        '***REMOVED***');
-define('SECURE_AUTH_SALT', '***REMOVED***');
-define('LOGGED_IN_SALT',   '***REMOVED***');
-define('NONCE_SALT',       '***REMOVED***');
 
 // ==============================================================
 // Table prefix
