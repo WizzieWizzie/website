abde61c62138c7dbf6ce8866697b4005ae769e0a
David Sutherland <sutherland.dave@gmail.com>
2017-02-19 15:10:50 +0000

    ocd

--- puppet/manifests/www.pp
+++ puppet/manifests/www.pp
@@ -3,31 +3,31 @@ Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
 
 class www {
 
-    hostname { 'set the www server hostname':
-      server  => 'www'
-    }
+  hostname { 'set the www server hostname':
+    server  => 'www'
+  }
 
-    include mysql_server
+  include mysql_server
 
-    class { 'nginx_server':
-      server  => 'www'
-    }
+  class { 'nginx_server':
+    server  => 'www'
+  }
 
-    include wordpress_server
+  include wordpress_server
 
-    class { 'wizzie_wizzie_deployment_keys':
-      user    => 'admin'
-    }
+  class { 'wizzie_wizzie_deployment_keys':
+    user    => 'admin'
+  }
 
-    class { 'wizzie_wizzie_wordpress_deployment':
-      user    => 'admin',
-      group   => 'www-data',
-      server  => 'www',
-      require => [
-        Class['wordpress_server'],
-        Class['wizzie_wizzie_deployment_keys']
-      ]
-    }
+  class { 'wizzie_wizzie_wordpress_deployment':
+    user    => 'admin',
+    group   => 'www-data',
+    server  => 'www',
+    require => [
+      Class['wordpress_server'],
+      Class['wizzie_wizzie_deployment_keys']
+    ]
+  }
 
 }
 
